037db74798b58837921fccb312ae454e321ec8ac
smpark1020 <smpark911020@gmail.com>
2021-08-27 11:47:06 +0900

    Kth Largest Element in an Array

---
+++ src/com/smpaaark/leetcode/Q215.java
@@ -0,0 +1,20 @@
+package com.smpaaark.leetcode;
+
+import java.util.PriorityQueue;
+
+public class Q215 {
+
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+
+        for (int num : nums) {
+            heap.offer(num);
+            if (heap.size() > k) {
+                heap.poll();
+            }
+        }
+
+        return heap.poll();
+    }
+
+}
