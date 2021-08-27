47cfeeb84a1886f444608b432eba42b6a5839e5f
smpark1020 <smpark911020@gmail.com>
2021-08-27 10:56:56 +0900

    Evaluate Reverse Polish Notation

---
+++ src/com/smpaaark/leetcode/Q150.java
@@ -0,0 +1,42 @@
+package com.smpaaark.leetcode;
+
+import java.util.Stack;
+
+public class Q150 {
+
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : tokens) {
+            int rightNum = 0;
+            int leftNum = 0;
+            switch (token) {
+                case "+":
+                    rightNum = stack.pop();
+                    leftNum = stack.pop();
+                    stack.push(leftNum + rightNum);
+                    break;
+                case "-":
+                    rightNum = stack.pop();
+                    leftNum = stack.pop();
+                    stack.push(leftNum - rightNum);
+                    break;
+                case "*":
+                    rightNum = stack.pop();
+                    leftNum = stack.pop();
+                    stack.push(leftNum * rightNum);
+                    break;
+                case "/":
+                    rightNum = stack.pop();
+                    leftNum = stack.pop();
+                    stack.push(leftNum / rightNum);
+                    break;
+                default:
+                    stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.pop();
+    }
+
+}
