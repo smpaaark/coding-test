5c8d6d2fa1b5e1553020473c2bc8589f7a330b67
smpark1020 <smpark911020@gmail.com>
2021-08-30 23:40:40 +0900

    recursion(ing)

---
+++ src/com/smpaaark/leetcode/recursion/Q50.java
@@ -0,0 +1,28 @@
+package com.smpaaark.leetcode.recursion;
+
+public class Q50 {
+
+    public static void main(String[] args) {
+        Q50 q = new Q50();
+        System.out.println(q.myPow(2.00000, -2));
+//        System.out.println(q.myPow(0.00001, 2147483647));
+    }
+
+    public double myPow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        double temp = myPow(x,n / 2);
+        if (n % 2 == 0) {
+            return temp * temp;
+        } else {
+            if (n > 0) {
+                return x * temp * temp;
+            } else {
+                return (temp * temp) / x;
+            }
+        }
+    }
+
+}
