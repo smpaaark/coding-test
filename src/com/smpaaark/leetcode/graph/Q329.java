4e33bae320042e38e69ac7a6f2f2bcefa73f94ea
smpark1020 <smpark911020@gmail.com>
2021-09-01 23:38:25 +0900

    graph(ing)

---
+++ src/com/smpaaark/leetcode/graph/Q329.java
@@ -0,0 +1,67 @@
+package com.smpaaark.leetcode.graph;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Q329 {
+
+    int max = 0;
+    int[] dx = {-1, 1, 0, 0};
+    int[] dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) {
+        Q329 q = new Q329();
+        System.out.println(q.longestIncreasingPath(new int[][] {{7,8,9},{9,7,6},{7,2,3}}));
+    }
+
+    public int longestIncreasingPath(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] count = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (count[i][j] == 0) {
+                    solution(i, j, count, matrix);
+                }
+            }
+        }
+
+        return max;
+    }
+
+    private void solution(int x, int y, int[][] count, int[][] matrix) {
+        Queue<Point> queue = new LinkedList<>();
+        queue.offer(new Point(x, y, 1));
+
+        while (!queue.isEmpty()) {
+            Point point = queue.poll();
+            count[point.x][point.y] = point.count;
+            max = Math.max(max, point.count);
+            int nextCount = point.count + 1;
+
+            for (int i = 0; i < 4; i++) {
+                int tempX = point.x + dx[i];
+                int tempY = point.y + dy[i];
+                if (tempX >= 0 && tempY >= 0 && tempX < matrix.length && tempY < matrix[0].length && matrix[tempX][tempY] > matrix[point.x][point.y] && nextCount > count[tempX][tempY]) {
+                    queue.offer(new Point(tempX, tempY, nextCount));
+                    count[tempX][tempY] = nextCount;
+                }
+            }
+        }
+    }
+
+    class Point {
+
+        int x;
+        int y;
+        int count;
+
+        public Point(int x, int y, int count) {
+            this.x = x;
+            this.y = y;
+            this.count = count;
+        }
+
+    }
+
+}
--- src/com/smpaaark/leetcode/heap/Q378.java
+++ src/com/smpaaark/leetcode/heap/Q378.java
@@ -1,8 +1,37 @@
 package com.smpaaark.leetcode.heap;
 
+import java.util.PriorityQueue;
+
 public class Q378 {
 
     public int kthSmallest(int[][] matrix, int k) {
+        int n = matrix.length;
+        PriorityQueue<Point> heap = new PriorityQueue<>((a, b) -> a.val - b.val);
+        for (int i = 0; i < n; i++) {
+            heap.offer(new Point(0, i, matrix[0][i]));
+        }
+
+        while (k-- > 1) {
+            Point point = heap.poll();
+            if (point.x + 1 < n) {
+                heap.offer(new Point(point.x + 1, point.y, matrix[point.x + 1][point.y]));
+            }
+        }
+
+        return heap.poll().val;
+    }
+
+    class Point {
+
+        int x;
+        int y;
+        int val;
+
+        public Point(int x, int y, int val) {
+            this.x = x;
+            this.y = y;
+            this.val = val;
+        }
 
     }
 
