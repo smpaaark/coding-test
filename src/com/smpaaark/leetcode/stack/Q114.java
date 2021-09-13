df251a7d2380ca4a31fd26efc2f61de36c7c5fe1
smpark1020 <smpark911020@gmail.com>
2021-09-13 09:34:26 +0900

    stack

---
+++ src/com/smpaaark/leetcode/stack/Q114.java
@@ -0,0 +1,66 @@
+package com.smpaaark.leetcode.stack;
+
+public class Q114 {
+
+    public static void main(String[] args) {
+        Q114 q = new Q114();
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+
+        node1.left = node2;
+        node1.right = node5;
+
+        node2.left = node3;
+        node2.right = node4;
+
+        node5.right = node6;
+
+        q.flatten(node1);
+    }
+
+    private TreeNode prev = null;
+
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        flatten(root.right);
+        flatten(root.left);
+        root.right = prev;
+        root.left = null;
+        prev = root;
+    }
+
+    static class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+
+        TreeNode(int val) { this.val = val; }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public String toString() {
+            return "TreeNode{" +
+                    "val=" + val +
+                    ", left=" + left +
+                    ", right=" + right +
+                    '}';
+        }
+
+    }
+
+}
