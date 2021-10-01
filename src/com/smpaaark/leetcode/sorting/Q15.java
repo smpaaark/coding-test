723e2db559ae0b14c6bf9515ce4d92c36fb88545
smpark1020 <smpark911020@gmail.com>
2021-10-02 00:31:35 +0900

    stack2(ing)

--- src/com/smpaaark/leetcode/sorting/Q15.java
+++ src/com/smpaaark/leetcode/sorting/Q15.java
@@ -6,6 +6,11 @@ import java.util.List;
 
 public class Q15 {
 
+    public static void main(String[] args) {
+        Q15 q = new Q15();
+        System.out.println(q.threeSum(new int[]{-1,0,1,2,-1,-4}));
+    }
+
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
 
@@ -16,11 +21,33 @@ public class Q15 {
         Arrays.sort(nums);
 
         for (int i = 0; i < nums.length - 2; i++) {
-            int left = i + 1;
-            int right = nums.length -1;
+            if (i == 0 || nums[i] != nums[i - 1]){
+                int left = i + 1;
+                int right = nums.length - 1;
+
+                while (left < right) {
+                    int target = 0 - (nums[i] + nums[left]);
+                    if (nums[right] == target) {
+                        result.add(new ArrayList<>(Arrays.asList(nums[i], nums[left], nums[right])));
+                        while (left + 1 < right && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+
+                        while (right - 1 > left && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+                        left++;
+                        right--;
+                    } else if (nums[right] > target) {
+                        right--;
+                    } else {
+                        left++;
+                    }
+                }
+            }
         }
 
-        return null;
+        return result;
     }
 
 }
